7793bf41ddcc1e693fa87559449d928d4b4c862a
Tihitena <tihitenamesfin111@gmail.com>
2023-03-19 15:24:25 +0300

    wip

--- src/views/QuestionsView.vue
+++ src/views/QuestionsView.vue
@@ -1,16 +1,28 @@
 <template>
   <div>
+    <span>
+      <selector-item-vue :defaultValue="Category" :placeholder="Category" />
+      <selector-item-vue :defaultValue="Amount" :placeholder="Amount" />
+      <selector-item-vue :defaultValue="Type" :placeholder="Type" />
+      <selector-item-vue
+        :defaultValue="Difficulity"
+        :placeholder="Difficulity"
+      />
+    </span>
     <QuestionView />
   </div>
 </template>
 
 <script>
+import SelectorItemVue from "@/components/SelectorItem.vue";
 // @ is an alias to /src
 import QuestionView from "../components/AllQuestions.vue";
+
 export default {
   name: "HomeView",
   components: {
     QuestionView,
+    SelectorItemVue,
   },
 };
 </script>
